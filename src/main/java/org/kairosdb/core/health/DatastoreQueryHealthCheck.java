0d8a72de7d17b8e7d36397b0e39cc88a43eb977d
Bruno Ballekens <bruno@waylay.io>
2018-01-12 11:37:48 +0100

    Backport of #17bdac7 to 1.1.3: Fix datastore health check to not query the entire universe

--- src/main/java/org/kairosdb/core/health/DatastoreQueryHealthCheck.java
+++ src/main/java/org/kairosdb/core/health/DatastoreQueryHealthCheck.java
@@ -2,39 +2,55 @@ package org.kairosdb.core.health;
 
 import com.codahale.metrics.health.HealthCheck;
 import com.google.inject.Inject;
-import org.kairosdb.core.datastore.Datastore;
+import org.kairosdb.core.Main;
+import org.kairosdb.core.datastore.DataPointGroup;
+import org.kairosdb.core.datastore.DatastoreQuery;
+import org.kairosdb.core.datastore.KairosDatastore;
+import org.kairosdb.core.datastore.QueryMetric;
 import org.kairosdb.core.exception.DatastoreException;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-public class DatastoreQueryHealthCheck extends HealthCheck implements HealthStatus
-{
-	static final String NAME = "Datastore-Query";
-	private final Datastore datastore;
+public class DatastoreQueryHealthCheck extends HealthCheck implements HealthStatus {
+    static final String NAME = "Datastore-Query";
+    private final KairosDatastore datastore;
+    private org.slf4j.Logger logger = LoggerFactory.getLogger(Main.class);
 
-	@Inject
-	public DatastoreQueryHealthCheck(Datastore datastore)
-	{
-		this.datastore = checkNotNull(datastore);
-	}
+    @Inject
+    public DatastoreQueryHealthCheck(KairosDatastore datastore) {
+        this.datastore = checkNotNull(datastore);
+    }
 
-	@Override
-	public String getName()
-	{
-		return NAME;
-	}
+    @Override
+    public String getName() {
+        return NAME;
+    }
 
-	@Override
-	protected Result check() throws Exception
-	{
-		try
-		{
-			datastore.getMetricNames();
-			return Result.healthy();
-		}
-		catch (DatastoreException e)
-		{
-			return Result.unhealthy(e);
-		}
-	}
+    @Override
+    protected Result check() throws Exception {
+        DatastoreQuery query = null;
+        try {
+            logger.debug("Checking datastore health...");
+            query = datastore.createQuery(
+                    new QueryMetric(System.currentTimeMillis() - (10 * 60 * 1000),
+                            0, "kairosdb.jvm.thread_count"));
+            List<DataPointGroup> results = query.execute();
+            logger.debug(" ... got results (size:" + results.size() + ")");
+            return Result.healthy();
+        } catch (DatastoreException e) {
+            logger.warn("Got exception : " + e.getMessage(), e);
+            return Result.unhealthy(e);
+        } finally {
+            if (query!=null) {
+                try {
+                    query.close();
+                } catch (Exception ex) {
+                    logger.error("Caught exception while closing query", ex);
+                }
+            }
+        }
+    }
 }
