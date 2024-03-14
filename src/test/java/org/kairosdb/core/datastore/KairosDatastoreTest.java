2f1b18f7fbcd681c977d4a29af821d90a9d1a472
ramil <ramazanyich@gmail.com>
2024-03-14 10:08:22 +0100

    fix layout

--- src/test/java/org/kairosdb/core/datastore/KairosDatastoreTest.java
+++ src/test/java/org/kairosdb/core/datastore/KairosDatastoreTest.java
@@ -192,13 +192,13 @@ public class KairosDatastoreTest
 		DatastoreQuery dq = datastore.createQuery(metric);
 		List<DataPointGroup> results = dq.execute();
 
-    	DataPointGroup group = results.get(0);
+		DataPointGroup group = results.get(0);
 		assertThat(group.hasNext(), equalTo(true));
 		DataPoint dataPoint = group.next();
 		assertThat(dataPoint.getTimestamp(), equalTo(1001000L));
 		assertThat(dataPoint.getLongValue(), equalTo(6L));
 		assertThat(group.hasNext(), equalTo(false));
-        dq.close();
+		dq.close();
 	}
 
 	@Test
